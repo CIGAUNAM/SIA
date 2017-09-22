b14894d6f323d38ea64eeda84ccca553a4786402
Nizhi Saeba <mathereall@gmail.com>
2017-09-21 20:33:13 -0500

    libros

---
+++ migracion2.py
@@ -0,0 +1,25 @@
+import os
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "SIA.settings")
+
+django.setup()
+
+from datetime import datetime, date
+from django.db import models
+# from django.contrib.auth.models import AbstractUser
+from django.conf import settings
+
+# from autoslug import AutoSlugField
+from nucleo.models import *
+from apoyo_institucional.models import Comision, Representacion, CargoAcademicoAdministrativo, \
+    RepresentacionOrganoColegiado, ComisionAcademica, ApoyoTecnico, ApoyoOtraActividad
+from desarrollo_tecnologico.models import TipoDesarrollo, Licencia, DesarrolloTecnologico
+from difusion_cientifica.models import MemoriaInExtenso, PrologoLibro, Resena, OrganizacionEventoAcademico, \
+    ParticipacionEventoAcademico
+
+from formacion_academica.models import CursoEspecializacion, Licenciatura, Maestria, Doctorado, PostDoctorado
+from experiencia_laboral.models import *
+
+import uuid
+
