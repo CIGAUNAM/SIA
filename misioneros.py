a3d39e676184697c87b9fc735b3add2d411c27db
Nizhi Saeba <mathereall@gmail.com>
2017-12-06 16:45:33 -0600

    libros

--- misioneros.py
+++ misioneros.py
@@ -1,99 +1,112 @@
+
+
 lado_izquierdo = 0
 lado_derecho = 1
 balsa = 2
 misioneros = 0
 canibales = 1
 
-# en el estado inicial todos están del lado izquierdo y nadie del lado derecho, y la canoa está del lado izquierdo
+# todos a la izquierda, y la posición de la balsa
 estado_inicial = [[3, 3], [0, 0], lado_izquierdo]
+# todos a la derecha, y la posición de la balsa
+estado_final = [[0, 0], [3, 3], lado_derecho]
 
-# en el estado objetivo todos están del lado derecho y nadie del lado izquierdo, y la canoa está del lado derecho
-estado_objetivo = [[0, 0], [3, 3], lado_derecho]
+# viajes posibles (misionero, caníbal)
+opciones_viajes = [[1, 0], [0, 1], [1, 1], [2, 0], [0, 2]]
 
-# consideramos los viajes posibles en una lista de estados, donde cada estado es una lista y donde el orden importa,
-# siendo cada sublista una posibilidad de la canoa, con el elemento [0] el misionero, y el elemento [1] el canibal
-viajes = [[1, 0], [0, 1], [1, 1], [2, 0], [0, 2]]
+viajes = []
+camino = []
+estado = estado_inicial
+posibles = opciones_viajes.copy()
 
+
+# devuelve si un estado es válido o no
 def es_estado_valido(estado):
-    # invalido si hay de más o de menos
+    # que no hayan de más o de menos
     if estado[lado_izquierdo][misioneros] > 3 or estado[lado_izquierdo][misioneros] < 0 or estado[lado_derecho][misioneros] > 3 or estado[lado_derecho][misioneros] < 0 or estado[lado_izquierdo][canibales] > 3 or estado[lado_izquierdo][canibales] < 0 or estado[lado_derecho][canibales] > 3 or estado[lado_derecho][canibales] < 0:
         return False
-    # Que no haya mas canibales que misioneros del lado izquierdo
+    # que no haya mas canibales que misioneros del lado izquierdo
     elif estado[lado_izquierdo][misioneros] != 0 and estado[lado_izquierdo][misioneros] < estado[lado_izquierdo][canibales]:
         return False
-    # Que no haya mas canibales que misioneros del lado derecho
+        # que no haya mas canibales que misioneros del lado derecho
     elif estado[lado_derecho][misioneros] != 0 and estado[lado_derecho][misioneros] < estado[lado_derecho][canibales]:
         return False
-
-    # si no se cumple ninguno de los anteriores, entonces el estado es valido
-    else:
-        return True
+    return True
 
 
-def canoa(estado, viaje):
-    estado[estado[balsa]][misioneros] = estado[estado[balsa]][misioneros] - viaje[misioneros]
-    estado[estado[balsa]][canibales] = estado[estado[balsa]][canibales] - viaje[canibales]
+# cambia el estado (la posición) de la balsa según el viaje
+def estado_balsa(estado, viaje):
+    estado[estado[balsa]][misioneros] -= viaje[misioneros]
+    estado[estado[balsa]][canibales] -= viaje[canibales]
 
     if estado[balsa] == lado_izquierdo:
         estado[balsa] = lado_derecho
     else:
         estado[balsa] = lado_izquierdo
 
-    estado[estado[balsa]][misioneros] = estado[estado[balsa]][misioneros] + viaje[misioneros]
-    estado[estado[balsa]][canibales] = estado[estado[balsa]][canibales] + viaje[canibales]
+    estado[estado[balsa]][misioneros] += viaje[misioneros]
+    estado[estado[balsa]][canibales] += viaje[canibales]
 
 
-def imprimir_estado(estado):
-    espacio = "-----------"
-    print("Misioneros:{}, Canibales:{} {} Misioneros:{}, Canibales:{}").format(estado[lado_izquierdo][misioneros], estado[lado_izquierdo][canibales], espacio, estado[lado_derecho][misioneros], estado[lado_derecho][canibales])
+def mostrar_estado(estado):
+    b = ''
+    if estado[balsa] == 0:
+        b = "--w__--"
+    elif estado[balsa] == 1:
+        b = "--__w--"
+    else:
+        b = "--_e_--"  # este no deberia pasar nunca, pero por si si, la e es de error xD
+    print(estado[lado_izquierdo][misioneros], estado[lado_izquierdo][canibales], b, estado[lado_derecho][misioneros], estado[lado_derecho][canibales])
 
 
-viajes_aux = []
-anterior = []
+# mientras el estado actual no sea igual al estado final (o sea, que no se haya llegado al estado objetivo)
+while estado != estado_final:
+    if len(posibles) == 0:
+        exit(1)
 
-
-# empezar con estado inicial:
-estado = estado_inicial.copy()
-posibles = viajes.copy()
-
-
-while estado != estado_objetivo:
-    posibleEstado = None
-    if posibles is None:
-        print("NO he encontrado solucion!")
-        break
-
-    # probamos viajes hasta que se nos acaben o uno sea válido
-    while len(posibles) > 0:
+    # probamos viajes hasta que se nos acaben los viajes posibles o uno sea válido
+    while posibles:
         viaje = posibles.pop(0)
 
-        # duplicamos los vectores internos
-        posibleEstado = [estado[lado_izquierdo].copy(), estado[lado_derecho].copy(), estado[balsa]]
-        print(posibleEstado)
-        print("aqui")
-        canoa(posibleEstado, viaje)
+        posible_estado = [estado[lado_izquierdo].copy(), estado[lado_derecho].copy(), estado[balsa]]
+        estado_balsa(posible_estado, viaje)
 
-        # salimos si es válido y no se ha repetido
-        print(anterior.index(posibleEstado))
-        if es_estado_valido(posibleEstado) and anterior.index(posibleEstado) == None:
+        # salimos del bucle si el estado es válido y no existe ya en el camino
+        if es_estado_valido(posible_estado) and posible_estado not in camino:
             break
 
-        # no es válido
-        posibleEstado = None
+        # si todo lo anterior falla, entonces el estado no era bueno
+        posible_estado = None
 
 
-    # tenemos uno válido?
-    if posibleEstado:
-        # nos acordamos del estado actual
-        anterior.append(estado)
+    if posible_estado:
+        # registramos del estado actual
+        camino.append(estado)
         viajes.append(posibles)
 
-        # aceptamos el viaje como bueno (por ahora)
-        estado = posibleEstado
-        posibles = viajes.copy()
+        # el viaje parece bueno
+        estado = posible_estado
+        posibles = opciones_viajes.copy()
     else:
-        # el viaje no nos ha llevado a un resultado
-        # asi que recuperamos el estado anterior
-        estado = anterior.pop()
+        # el viaje no dio resultado satisfactorio, recuperamos el estado anterior, quitando del camino el ultimo estado,
+        # que no funcionó
+        estado = camino.pop()
         posibles = viajes.pop()
-        posibleEstado = None
+        posible_estado = None
+
+
+
+print(camino)
+
+while True:
+    try:
+        res = camino.pop(0)
+    except:
+        break
+    mostrar_estado(res)
+mostrar_estado(estado)
+
+
+
+
+
