05eeb12f3e81b528ae4706a48f40bb7c13a8dffb
Nizhi Saeba <mathereall@gmail.com>
2018-01-11 18:57:46 -0600

    libros

--- csp.py
+++ csp.py
@@ -87,3 +87,110 @@ colors  = {'Rojo', 'Verde', 'Azul', 'Naranja'}
 
 resolver_problema(australia, colors)
 print("\npasos para encontrar la solución:", contador)
+
+
+
+
+
+
+
+# De importan las bibliotecas shapefile y BeautifulSoup, para generar archivos shapefile
+# y leer documentos SGML respectivamente.
+import shapefile
+from bs4 import BeautifulSoup
+
+
+doc = open('doc.kml')  # leer el archivo .kml y guardar su contenido en la variable doc
+soup = BeautifulSoup(doc, "html.parser")  # leer mediante BeautifulSoup el archivo contenido en doc, como html
+places = soup.find_all('placemark')
+
+w = shapefile.Writer(shapefile.POINT)
+# w.autoBalance = 1
+w.field('ident', 'C', 100)
+w.field('dap', 'F', 10, 8)
+w.field('dbas', 'F', 10, 8)
+w.field('cob', 'F', 10, 8)
+w.field('alt', 'F', 10, 8)
+w.field('copa', 'N')
+w.field('densidad', 'N')
+w.field('ffuste', 'N')
+w.field('ifuste', 'N')
+w.field('esalud', 'N')
+w.field('ired', 'N')
+w.field('dinf', 'N')
+
+for i in range(len(places)):
+    try:
+        ident = places[i].ident.get_text().replace('\n', '')
+    except:
+        ident = ''
+
+    try:
+        dap = float(places[i].dap.get_text().replace('\n', ''))
+    except:
+        dap = 0.0
+
+    try:
+        dbas = float(places[i].dbas.get_text().replace('\n', ''))
+    except:
+        dbas = 0.0
+
+    try:
+        cob = float(places[i].cob.get_text().replace('\n', ''))
+    except:
+        cob = 0.0
+
+    try:
+        alt = float(places[i].alt.get_text().replace('\n', ''))
+    except:
+        alt = 0.0
+
+    try:
+        copa = int(places[i].copa.get_text().replace('\n', ''))
+    except:
+        copa = 0
+
+    try:
+        densidad = int(places[i].densidad.get_text().replace('\n', ''))
+    except:
+        densidad = 0
+
+    try:
+        ffuste = int(places[i].ffuste.get_text().replace('\n', ''))
+    except:
+        ffuste = 0
+
+    try:
+        ifuste = int(places[i].ifuste.get_text().replace('\n', ''))
+    except:
+        ifuste = 0
+
+    try:
+        esalud = int(places[i].esalud.get_text().replace('\n', ''))
+    except:
+        esalud = 0
+
+    try:
+        ired = int(places[i].ired.get_text().replace('\n', ''))
+    except:
+        ired = 0
+
+    try:
+        dinf = int(places[i].dinf.get_text().replace('\n', ''))
+    except:
+        dinf = 0
+
+    try:
+        coord = places[i].coordinates.get_text().split(',')
+        coord[0] = float(coord[0])
+        coord[1] = float(coord[1])
+    except:
+        coord = [0, 0]
+
+    print(coord[0], coord[1])
+    w.point(coord[0], coord[1])
+    print(ident, dap, dbas, cob, alt, copa, densidad, ffuste, ifuste, esalud, ired, dinf)
+    w.record(ident, dap, dbas, cob, alt, copa, densidad, ffuste, ifuste, esalud, ired, dinf)
+
+w.save("arboles.shp")
+
