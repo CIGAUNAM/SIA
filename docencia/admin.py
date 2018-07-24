2eed7ae66bece436f6cd72e887304b159fbde8af
mathereall@gmail.com <LifeStr34m>
2018-07-24 16:30:10 -0500

    correccion decimales factor de impacto en revista

--- docencia/admin.py
+++ docencia/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 
-from . models import CursoDocenciaEscolarizado
+from . models import CursoDocenciaEscolarizado, CursoDocenciaExtracurricular
 
 admin.site.register(CursoDocenciaEscolarizado)
+admin.site.register(CursoDocenciaExtracurricular)
