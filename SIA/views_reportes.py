08e8f2660b7a12b2bde3ac899b7b2718f1b70d64
mathereall@gmail.com <LifeStr34m>
2018-08-14 18:04:10 -0500

    correccion decimales factor de impacto en revista

--- SIA/views_reportes.py
+++ SIA/views_reportes.py
@@ -52,9 +52,17 @@ class Informe(View):
     context['libros_investigacion_nacionales_enprensa'] = Libro.objects.filter(tipo='INVESTIGACION', fecha__year=this_year, status='EN_PRENSA').filter(Q(autores__isnull=False) | Q(editores__isnull=False) | Q(coordinadores__isnull=False)).filter(editorial__pais__nombre='México').distinct()
     context['libros_investigacion_nacionales_aceptados'] = Libro.objects.filter(tipo='INVESTIGACION', fecha__year=this_year, status='ACEPTADO').filter(Q(autores__isnull=False) | Q(editores__isnull=False) | Q(coordinadores__isnull=False)).filter(editorial__pais__nombre='México').distinct()
 
+    context['libros_investigacion_internacionales_publicados'] = Libro.objects.filter(tipo='INVESTIGACION', fecha__year=this_year, status='PUBLICADO').filter(Q(autores__isnull=False) | Q(editores__isnull=False) | Q(coordinadores__isnull=False)).exclude(editorial__pais__nombre='México').distinct()
+    context['libros_investigacion_internacionales_enprensa'] = Libro.objects.filter(tipo='INVESTIGACION', fecha__year=this_year, status='EN_PRENSA').filter(Q(autores__isnull=False) | Q(editores__isnull=False) | Q(coordinadores__isnull=False)).exclude(editorial__pais__nombre='México').distinct()
+    context['libros_investigacion_internacionales_aceptados'] = Libro.objects.filter(tipo='INVESTIGACION', fecha__year=this_year, status='ACEPTADO').filter(Q(autores__isnull=False) | Q(editores__isnull=False) | Q(coordinadores__isnull=False)).exclude(editorial__pais__nombre='México').distinct()
+
     context['capitulos_libros_investigacion_nacionales_publicados'] = CapituloLibroInvestigacion.objects.filter(libro__fecha__year=this_year, libro__status='PUBLICADO').filter(libro__editorial__pais__nombre='México').distinct()
     context['capitulos_libros_investigacion_nacionales_enprensa'] = CapituloLibroInvestigacion.objects.filter(libro__fecha__year=this_year, libro__status='EN_PRENSA').filter(libro__editorial__pais__nombre='México').distinct()
     context['capitulos_libros_investigacion_nacionales_enviados'] = CapituloLibroInvestigacion.objects.filter(libro__fecha__year=this_year, libro__status='ENVIADO').filter(libro__editorial__pais__nombre='México').distinct()
+
+    context['capitulos_libros_investigacion_internacionales_publicados'] = CapituloLibroInvestigacion.objects.filter(libro__fecha__year=this_year, libro__status='PUBLICADO').exclude(libro__editorial__pais__nombre='México').distinct()
+    context['capitulos_libros_investigacion_internacionales_enprensa'] = CapituloLibroInvestigacion.objects.filter(libro__fecha__year=this_year, libro__status='EN_PRENSA').exclude(libro__editorial__pais__nombre='México').distinct()
+    context['capitulos_libros_investigacion_internacionales_enviados'] = CapituloLibroInvestigacion.objects.filter(libro__fecha__year=this_year, libro__status='ENVIADO').exclude(libro__editorial__pais__nombre='México').distinct()
 
     def get(self, request):
         return render(request, self.template_name, self.context)
