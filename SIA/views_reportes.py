5f88244ed330780413167852159c647c7e9aadf7
Nizhi Saeba <mathereall@gmail.com>
2019-01-23 18:22:17 -0600

    correccion decimales factor de impacto en revista|

--- SIA/views_reportes.py
+++ SIA/views_reportes.py
@@ -100,6 +100,8 @@ class Informe(View):
     context['participacion_eventos_academicos_nacionales_participacion'] = ParticipacionEventoAcademico.objects.filter(evento__fecha_inicio__year=this_year, por_invitacion=False).filter(evento__pais__nombre='México').distinct()
     context['participacion_eventos_academicos_internacionales_invitacion'] = ParticipacionEventoAcademico.objects.filter(evento__fecha_inicio__year=this_year, por_invitacion=True).exclude(evento__pais__nombre='México').distinct()
     context['participacion_eventos_academicos_internacionales_participacion'] = ParticipacionEventoAcademico.objects.filter(evento__fecha_inicio__year=this_year, por_invitacion=False).exclude(evento__pais__nombre='México').distinct()
+    context['participacion_eventos_academicos_nacionales'] = ParticipacionEventoAcademico.objects.filter(evento__fecha_inicio__year=this_year).filter(evento__pais__nombre='México').distinct()
+    context['participacion_eventos_academicos_internacionales'] = ParticipacionEventoAcademico.objects.filter(evento__fecha_inicio__year=this_year).exclude(evento__pais__nombre='México').distinct()
 
     context['organizacion_eventos_academicos'] = OrganizacionEventoAcademico.objects.filter(evento__fecha_inicio__year=this_year).distinct()
     context['organizacion_eventos_divulgacion'] = OrganizacionEventoDivulgacion.objects.filter(evento__fecha_inicio__year=this_year).distinct()
