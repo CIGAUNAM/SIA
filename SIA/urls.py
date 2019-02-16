3175f00662141fc6714bdafaa44a6201869cc8b5
Nizhi Saeba <mathereall@gmail.com>
2019-02-15 20:39:08 -0600

    correccion decimales factor de impacto en revista|

--- SIA/urls.py
+++ SIA/urls.py
@@ -18,19 +18,17 @@ from django.contrib import admin
 
 from django.contrib.auth import views as auth_views
 
-#from nucleo.views import inicio
 from nucleo.views import *
 
 from django.conf.urls.static import static
-from django.conf.urls import url
 from SIA.views import *
 from SIA.views_reportes import *
 
 
 
 urlpatterns = [
-    url(r'^login/$', auth_views.login, {'template_name': 'adminlte/login.html'}, name='login'),
-    url(r'^logout/$', auth_views.logout, {'template_name': 'adminlte/login.html'}, name='logout'),
+    url(r'^login/$', auth_views.LoginView.as_view(template_name='adminlte/login.html'), name='login'),
+    url(r'^logout/$', auth_views.LogoutView.as_view(template_name='adminlte/login.html'), name='logout'),
 
     #url(r'^password_change/$', auth_views.password_change, name='password_change'),
     #url(r'^password_change/done/$', auth_views.password_change_done, name='password_change_done'),
