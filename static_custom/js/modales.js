f0a1922572860cda40f61a9a73bb3415b5e956b1
Nizhi Saeba <mathereall@gmail.com>
2019-02-18 20:26:54 -0600

    correccion decimales factor de impacto en revista|

--- static_custom/js/modales.js
+++ static_custom/js/modales.js
@@ -1,108 +1,87 @@
 agregar_dependencia_dialog = null
 $(function () {
 
-    agregar_institucion_dialog = $("#agregar-institucion").dialog({
-        autoOpen: false,
-        height: 700,
-        width: 900,
-        modal: true,
-        buttons: {
-            agregar: {
-                text: "Agregar institución",
-                click: function () {
-                    $("#modal_form_institucion_agregar").submit()
+    /*
+        agregar_dependencia_dialog = $("#agregar-dependencia").dialog({
+            autoOpen: false,
+            height: 700,
+            width: 900,
+            modal: true,
+            buttons: {
+                agregar: {
+                    text: "Agregar dependencia",
+                    click: function () {
+                        $("#modal_form_dependencia_agregar").submit()
+                    },
+                    class: 'btn btn-success ui-button-left'
                 },
-                class: 'btn btn-success ui-button-left'
+                cancelar: {
+                    text: "Cancelar",
+                    click: function () {
+                        $(this).dialog("close")
+                    },
+                    class: 'btn btn-primary ui-button-left'
+                }
             },
-            cancelar: {
-                text: "Cancelar",
-                click: function () {
-                    $(this).dialog("close")
-                },
-                class: 'btn btn-primary ui-button-left'
+
+            open: function (event, ui) {
+                $('#agregar-dependencia-modal-body').load('/nucleo/dependencias/agregar/', function () {
+                    $("#id_institucion_dependencia").djangoSelect2({dropdownParent: $("#agregar-dependencia")});
+                    $("#id_subsistema_unam_dependencia").djangoSelect2({dropdownParent: $("#agregar-dependencia")});
+                });
             }
-        },
+        });
+    */
 
-        open: function (event, ui) {
-            $('#agregar-institucion-modal-body').load('/nucleo/instituciones/agregar/', function () {
-                $("#id_institucion_pais").djangoSelect2({dropdownParent: $("#agregar-institucion")});
-                $("#id_institucion_clasificacion").djangoSelect2({dropdownParent: $("#agregar-institucion")});
-                $("#id_institucion_subsistemaunam").djangoSelect2({dropdownParent: $("#agregar-institucion")});
-            });
-        }
-    });
+    $("#boton-agregar-institucion").on("click", function (e) {
+        $('#d_subsunam').hide();
 
-    detalle_institucion_dialog = $("#detalle-institucion").dialog({
-        autoOpen: false,
-        height: 700,
-        width: 900,
-        modal: true,
-        buttons: {
-            agregar: {
-                text: "Agregar institución",
-                click: function () {
-                    $("#modal_form_institucion_agregar").submit()
+        agregar_institucion_dialog = $("#agregar-institucion").dialog({
+            autoOpen: false,
+            height: 700,
+            width: 900,
+            modal: true,
+            buttons: {
+                agregar: {
+                    text: "Agregar institución",
+                    click: function () {
+                        $("#modal_form_institucion_agregar").submit()
+                    },
+                    class: 'btn btn-success ui-button-left'
                 },
-                class: 'btn btn-success ui-button-left'
+                cancelar: {
+                    text: "Cancelar",
+                    click: function () {
+                        $(this).dialog("close")
+                    },
+                    class: 'btn btn-primary ui-button-left'
+                }
             },
-            cancelar: {
-                text: "Cancelar",
-                click: function () {
-                    $(this).dialog("close")
-                },
-                class: 'btn btn-primary ui-button-left'
+
+            open: function (event, ui) {
+                $('#agregar-institucion-modal-body').load('/nucleo/instituciones/agregar/', function () {
+                    $("#id_institucion_pais").djangoSelect2({dropdownParent: $("#agregar-institucion")});
+                    $("#id_institucion_clasificacion").djangoSelect2({dropdownParent: $("#agregar-institucion")});
+                    $("#id_institucion_subsistemaunam").djangoSelect2({dropdownParent: $("#agregar-institucion")});
+                });
             }
-        },
+        });
 
-        open: function (event, ui) {
-            //$('#agregar-institucion-modal-body').load('/nucleo/instituciones/agregar/', function () {
-
-            $('#detalle-institucion-modal-body').load(('/nucleo/instituciones/' + $("#id_institucion").val().toString()) + "/", function () {
-
-                $("#id_institucion_pais").djangoSelect2({dropdownParent: $("#agregar-institucion")});
-                $("#id_institucion_clasificacion").djangoSelect2({dropdownParent: $("#agregar-institucion")});
-                $("#id_institucion_subsistemaunam").djangoSelect2({dropdownParent: $("#agregar-institucion")});
-            });
-        }
-    });
-
-    agregar_dependencia_dialog = $("#agregar-dependencia").dialog({
-        autoOpen: false,
-        height: 700,
-        width: 900,
-        modal: true,
-        buttons: {
-            agregar: {
-                text: "Agregar dependencia",
-                click: function () {
-                    $("#modal_form_dependencia_agregar").submit()
-                },
-                class: 'btn btn-success ui-button-left'
-            },
-            cancelar: {
-                text: "Cancelar",
-                click: function () {
-                    $(this).dialog("close")
-                },
-                class: 'btn btn-primary ui-button-left'
+        $("#id_institucion_perteneceunam").on('change', function () {
+            console.log($("#id_institucion_perteneceunam").is(':checked'));
+            if ($("#id_institucion_perteneceunam").is(':checked')) {
+                $('#d_subsunam').show('slow');
+                $('#id_institucion_subsistemaunam').attr("required", "true");
+            } else {
+                console.log("esconder cambio")
+                $('#d_subsunam').hide('slow');
+                $('#id_institucion_subsistemaunam').val(null).trigger('change');
+                $('#id_institucion_subsistemaunam').attr("required", null);
             }
-        },
-
-        open: function (event, ui) {
-            $('#agregar-dependencia-modal-body').load('/nucleo/dependencias/agregar/', function () {
-                $("#id_institucion_dependencia").djangoSelect2({dropdownParent: $("#agregar-dependencia")});
-                $("#id_subsistema_unam_dependencia").djangoSelect2({dropdownParent: $("#agregar-dependencia")});
-            });
-        }
-    });
+        });
 
 
-    $("#boton-agregar-dependencia").on("click", function () {
-        agregar_dependencia_dialog.dialog("open");
-    });
 
-    $("#boton-agregar-institucion").on("click", function () {
-        //alert("instituciosdkfhbdjksfb")
         agregar_institucion_dialog.dialog("open");
     });
 
@@ -114,23 +93,47 @@ $(function () {
             console.log("no item")
         } else {
             console.log($("#id_dependencia").val())
+            detalle_institucion_dialog = $("#detalle-institucion").dialog({
+                autoOpen: false,
+                height: 700,
+                width: 900,
+                modal: true,
+                buttons: {
+                    agregar: {
+                        text: "Agregar institución",
+                        click: function () {
+                            $("#modal_form_institucion_detalle").submit()
+                        },
+                        class: 'btn btn-success ui-button-left'
+                    },
+                    cancelar: {
+                        text: "Cancelar",
+                        click: function () {
+                            $(this).dialog("close")
+                        },
+                        class: 'btn btn-primary ui-button-left'
+                    }
+                },
+
+                open: function (event, ui) {
+                    //$('#agregar-institucion-modal-body').load('/nucleo/instituciones/agregar/', function () {
+
+                    $('#detalle-institucion-modal-body').load(('/nucleo/instituciones/' + $("#id_institucion").val().toString()) + "/", function () {
+                        console.log("detalle start")
+                        $("#id_institucion_pais").djangoSelect2({dropdownParent: $("#agregar-institucion")});
+                        $("#id_institucion_clasificacion").djangoSelect2({dropdownParent: $("#agregar-institucion")});
+                        $("#id_institucion_subsistemaunam").djangoSelect2({dropdownParent: $("#agregar-institucion")});
+
+                    });
+                    e.stopPropagation();
+
+                }
+            });
+            console.log("aqui")
             detalle_institucion_dialog.dialog("open");
         }
 
     });
 
 
-    $('#boton-ver-dependencia').on('click', function (e) {
-        if ($("#id_dependencia").val() === null) {
-            e.stopPropagation();
-        } else {
-            console.log($("#id_dependencia").val())
-            $("#detalle-dependencia").modal()
-            $("#detalle-dependencia").removeData()
-            $('#detalle-dependencia-modal-body').html('');
-            $('#detalle-dependencia-modal-body').load(('/nucleo/dependencias/' + $("#id_dependencia").val().toString()) + "/", function () {
-                $("#id_institucion_dependencia").djangoSelect2({dropdownParent: $("#detalle-dependencia")});
-            })
-        }
-    })
 })
