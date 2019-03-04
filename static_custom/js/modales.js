dfdaa23331a2418024d4f4896499a9836274f49d
mathereall@gmail.com <LifeStr34m>
2019-03-04 08:59:37 -0600

    correccion decimales factor de impacto en revista|

--- static_custom/js/modales.js
+++ static_custom/js/modales.js
@@ -35,13 +35,12 @@ $(function () {
     */
 
     $("#boton-agregar-institucion").on("click", function (e) {
-
-
         agregar_institucion_dialog = $("#agregar-institucion").dialog({
             autoOpen: false,
             height: 700,
             width: 900,
             modal: true,
+            class: 'ui-button-left',
             buttons: {
                 agregar: {
                     text: "Agregar institución",
@@ -81,7 +80,6 @@ $(function () {
                 });
             }
         });
-
         agregar_institucion_dialog.dialog("open");
     });
 
@@ -138,7 +136,101 @@ $(function () {
             });
             detalle_institucion_dialog.dialog("open");
         }
+    });
 
+    /* ***** */
+
+    $("#boton-agregar-programalicenciatura").on("click", function (e) {
+        console.log("boton agregar programa licenciatura")
+        agregar_programalicenciatura_dialog = $("#agregar-programalicenciatura").dialog({
+            autoOpen: false,
+            height: 700,
+            width: 900,
+            modal: true,
+            class: 'ui-button-left',
+            buttons: {
+                agregar: {
+                    text: "Agregar programa de licenciatura",
+                    click: function () {
+                        $("#modal_form_programalicenciatura_agregar").submit()
+                    },
+                    class: 'btn btn-success ui-button-left'
+                },
+                cancelar: {
+                    text: "Cancelar",
+                    click: function () {
+                        $(this).dialog("close")
+                    },
+                    class: 'btn btn-primary ui-button-left'
+                }
+            },
+
+            open: function (event, ui) {
+                $('#agregar-programalicenciatura-modal-body').load('/nucleo/programas-licenciatura/agregar/', function () {
+                    $("#id_institucion_pais").djangoSelect2({dropdownParent: $("#agregar-programalicenciatura")});
+                    $("#id_institucion_clasificacion").djangoSelect2({dropdownParent: $("#agregar-programalicenciatura")});
+                    $("#id_institucion_subsistemaunam").djangoSelect2({dropdownParent: $("#agregar-programalicenciatura")});
+                    $('#d_subsunam').hide();
+
+                });
+            }
+        });
+        agregar_programalicenciatura_dialog.dialog("open");
+    });
+
+    $("#boton-detalle-institucion").on("click", function (e) {
+        if ($("#id_institucion").val() == '') {
+            e.stopPropagation();
+        } else {
+            detalle_institucion_dialog = $("#detalle-institucion").dialog({
+                autoOpen: false,
+                height: 700,
+                width: 900,
+                modal: true,
+                buttons: {
+                    actualizar: {
+                        text: "Actualizar institución",
+                        click: function () {
+                            $("#modal_form_institucion_detalle").submit()
+                        },
+                        class: 'btn btn-success ui-button-left'
+                    },
+                    cancelar: {
+                        text: "Cancelar",
+                        click: function () {
+                            $(this).dialog("close")
+                        },
+                        class: 'btn btn-primary ui-button-left'
+                    }
+                },
+
+                open: function (event, ui) {
+                    $('#detalle-institucion-modal-body').load(('/nucleo/instituciones/' + $("#id_institucion").val().toString()) + "/", function () {
+                        console.log("detalle start")
+                        $("#id_institucion_pais").djangoSelect2({dropdownParent: $("#agregar-institucion")});
+                        $("#id_institucion_clasificacion").djangoSelect2({dropdownParent: $("#agregar-institucion")});
+                        $("#id_institucion_subsistemaunam").djangoSelect2({dropdownParent: $("#agregar-institucion")});
+
+                        $("#id_institucion_perteneceunam").on('change', function () {
+                            console.log($("#id_institucion_perteneceunam").is(':checked'));
+                            if ($("#id_institucion_perteneceunam").is(':checked')) {
+                                $('#d_subsunam').show('slow');
+                                $('#id_institucion_subsistemaunam').attr("required", "true");
+                            } else {
+                                console.log("esconder cambio")
+                                $('#d_subsunam').hide('slow');
+                                $('#id_institucion_subsistemaunam').val(null).trigger('change');
+                                $('#id_institucion_subsistemaunam').attr("required", null);
+                            }
+                        });
+
+                    });
+                    e.stopPropagation();
+
+                }
+            });
+            detalle_institucion_dialog.dialog("open");
+        }
     });
 
 
